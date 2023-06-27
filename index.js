5e2af3e1ea629d4a46601155695f88ae423073d6
Finn-jf <finnfassnacht@gmail.com>
2023-06-27 16:14:11 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const nominatim = require('nominatim-client');
+const client = nominatim.createClient({
+  useragent: "Heartbeat",             // The name of your application
+  referer: 'https://nominatim.openstreetmap.org/',  // The referer link
+});
+
+const express = require("express");
+const app = express();
+app.use(express.static("www"));
+app.use(express.json());
+PORT = 3000;
+app.post("/cords", (req,res) => {
+    async function re_addrs() {
+        var client_data = (req.body);
+        const query = {
+        lat: client_data.lat,
+        lon: client_data.lon
+        };
+        console.log(query)
+        result = await client.reverse(query)
+        res.send(result)
+        console.log(result)
+    }
+    re_addrs()
+});
+app.listen(PORT, () => {
+    console.log("Server up!");
+})
